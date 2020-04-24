d1fdba6ccff9a6eeea4d3369b82616b5bf3a4739
Charles Schiermeyer <matkatmusic@gmail.com>
2020-04-24 14:53:24 -0700

    Revised instructions

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@
 
 Create a branch named Part1
 
+Purpose:  This project will show you the difference between member functions and static class functions, as well as the difference between pointers and references and the code semantics that accompany pointers and references.
+
  Pointers
 
- Please look at the screenshot in the files to see what happens if you copy this assignment into Xcode
+ Please look at the screenshot in the files to see what happens if you paste this assignment into an existing cpp file in an xcode project
  
  Task: replace all of the <place holder> with proper names and types that will make this program compile and run.
  
- steps:
+ press the Compile, link and run... button and work your way through all of the error messages.
  
- 0) press the Compile, link and run... button and work your way through all of the error messages.
-         you can click on them to be taken to the appropriate line number.
+
+ steps:
  
  1) finish implementing the constructor for T. the constructor parameters need to initialize 2) and 3)
  
@@ -29,12 +31,14 @@ Create a branch named Part1
          see 8) for a hint about what type to use.
  
  6) finish instantiating the two T instances being declared in main(), based on the constructor you finished implementing in T
+     Their names (line 105,106) are different than the member variables names (line 77), even tho the placeholders share the same name.
+     Don't let this confuse you.
  
  7) Declare 'f' with the proper type so the call to f.compare works
  
  8) call f.compare() with the correct arguments, based on 2) and 5)
  
- 9) correct the cout that uses bigger's member variable with some safe pointer usage.
+ 9) correct the cout that uses smaller's member variable with some safe pointer usage.
          the possible return values of the compare() function should give you a hint.
  
  10) complete the implementation for the static function in <structName2>
@@ -43,11 +47,15 @@ Create a branch named Part1
  
  12) replicate the functionality of <structName2>'s static function by implementing a member function in U that does the same thing.
  
+ 13) remember the rules for using pointers!  What is the one thing we always do before we use a pointer?
+ 
  After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
- Send me the the link to your repl.it in a DM on Slack
+Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+
+send me a DM to check your pull request
 
  Wait for my code review.
  */
